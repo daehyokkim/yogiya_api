b08cdb51171b4a8837754de2b6b3a76516d2937e
김대혁 <holyjoly@gimdaehyeogs-MacBook-Pro.local>
2023-04-16 16:19:50 +0900

    add : 친구리스트 api 명세서 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import sign from "./sign/sgin.contoller.js";
 import auth from "./auth/auth.controller.js";
+import friendList from "./friendList/friend.controller.js";
 const router = Router();
 
 router.use("/v1/sign", sign);
 router.use("/v1/auth", auth);
+router.use("/v1/friends", friendList);
 router.get("/", (_, res) => {
   try {
     console.log("test");
