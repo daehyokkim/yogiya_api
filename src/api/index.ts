bb51b07a18831c0aaa41a6272951a0556fe2f6e1
김대혁 <holyjoly@gimdaehyeogs-MacBook-Pro.local>
2023-04-25 12:58:28 +0900

    update : socket통신 명세서 추가, 회의 내용기준으로 업데이트

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,11 +4,13 @@ import auth from "./auth/auth.controller";
 import friendList from "./friend/friend.controller";
 import myPage from "./myPage/myPage.controller";
 import authMiddleware from "../middleware/auth";
+import socket from "./socket/socket.controller";
 const router = Router();
 
 router.use("/v1/sign", sign);
 router.use("/v1/auth", auth);
 router.use("/v1/friends", authMiddleware, friendList);
 router.use("/v1/my-page", myPage);
+router.use("/socket", socket);
 
 export default router;
---
+++ src/api/socket/socket.controller.ts
@@ -0,0 +1,232 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get(
+  "/login",
+  () => {}
+
+  /*
+ #swagger.summary = "로그인 socket 통신"
+ #swagger.description = '**socket connection 연결 및 친구리스트를 리턴 받습니다** <br/> flutter endpoint는 미정입니다.
+ '
+ #swagger.responses[200] ={
+    description : '정상적으로 connection 연결시.. 단,fornt에서 endPoint 가 정해지면 가능함',
+   schema :{
+      data :{
+         friendList :[
+            {
+               nickName : '철수',
+               email : "이메일",
+               profile : "URL",
+               latitude : "위도",
+               longitude : "경도",
+               battery : "베터리",
+               speed : "속도"
+
+            }
+         ]
+      }
+   }
+
+ }
+*/
+);
+
+router.delete(
+  "/deleteFriend",
+  () => {}
+  /*
+     #swagger.summary = "친구 삭제 기능" 
+     #swagger.description = '
+     ** 친구리스트에서 친구 삭제를 담당하는 API입니다 ** <br/>
+     친구 삭제시 내룸,친구룸 모두 퇴장됩니다.
+     '
+     #swagger.parameters['obj']= {
+         in : 'body',
+         name : 'body',
+         description : '친구 email',
+         required: true,
+         schema:{
+             email : "친구 email"
+         }
+     }
+     #swagger.responses[200] ={
+         description : '친구 삭제 완료',
+         schema:{
+         error: false,
+         message :"SUCCESS"
+         }
+     }
+     */
+);
+
+router.post(
+  "/setRequest",
+  () => {}
+  /*
+     #swagger.summary = "요청 대기중인 친구 수락/거절 API"
+     #swagger.description = '
+     ** 요청 대기중인 친구를 수락/거절을 결정하는 API입니다 ** <br/>
+     친구 수락시 나에게 친구 정보를 보내는 endPoint 필요합니다.(미완)
+     '
+     #swagger.parameters['obj']= {
+         in : 'body',
+         name : 'body',
+         description : '친구 email, 수락(true)/거절(false)',
+         required: true,
+         schema:{
+             email : "친구 email",
+             answer : true
+            }
+        }
+     #swagger.responses[200] = {
+            description : "
+            친구 수락시\n
+                ```
+                data:{
+                    nickName : '철수',
+                    email : '이메일',
+                    profile : 'URL',
+                    latitude : '위도',
+                    longitude : '경도',
+                    battery : '베터리',
+                    speed : '속도',
+                }
+                ```\n\n
+            거절시\n
+                ```
+                data:{
+                ....음 이부분은 어떻게 보내주는게 편한지 회의해보기
+                }
+                ```
+         ",
+        }
+     */
+);
+
+router.put(
+  "/sendLocation",
+  () => {}
+
+  /*
+     #swagger.summary = "내위치 전송 통신"
+     #swagger.description = '**내 정보를 전송하고, 내 룸에 있는 친구들에게 브로드케스트 통신을 전송합니다.**
+     '
+    #swagger.parameters['obj']= {
+                in : 'body',
+                name : 'body',
+                discription: '변경할 비밀번호 데이터 전송',
+                required : true,
+                schema:{
+                    data :{
+                        latitude : '위도',
+                        longitude : '경도',
+                        battery : '베터리',
+                        speed : '속도',
+                    }
+                    
+                }
+        }
+     #swagger.responses[200] =  {
+        description : ' fornt에서 endPoint 가 정해지면 가능함',
+        schema :{
+               data :{
+                        latitude : '위도',
+                        longitude : '경도',
+                        battery : '베터리',
+                        speed : '속도',
+                    } 
+            }
+    
+        }
+    */
+);
+
+router.put(
+  "/updateProfile",
+  () => {}
+  /*
+          #swagger.summary = "프로필 변경 API (미완)"
+          #swagger.description = '
+          ** 내 프로필 정보를 변경하는 API 입니다. **'
+          #swagger.parameters['obj']= {
+              in : 'form-data',
+              name : 'form-data',
+              discription: '변경할 프로필 데이터 전송',
+              required : true,
+              schema:{
+                  newProfile : 'data..'
+              }
+          }
+          
+          #swagger.responses[200] = {
+              description : '정상적으로 프로필 변경 완료',
+              schema:{
+                  error : false,
+                  message : 'SUCCESS'
+              }
+          } 
+  
+          #swagger.responses[400] = {
+              description : '잘못된 데이터 전송 또는 접근으로 인한 오류 반환',
+              schema:{
+                  error: true,
+                  message : "잘못된 접근입니다."
+              }
+          }
+          #swagger.responses[500] = {
+              description : '서버 내부의 문제 발생',
+              schema:{
+                  error : true,
+                  message : "알수없은 문제 발생. 잠시후 다시 실행해주세요."
+              }
+          }
+    
+    */
+);
+
+router.put(
+  "/updatNickname",
+  () => {}
+  /*
+            #swagger.summary = "닉네임 변경 API (미완)"
+            #swagger.description = '
+            ** 내 닉네임 정보를 변경하는 API 입니다. **'
+            #swagger.parameters['obj']= {
+                in : 'form-data',
+                name : 'form-data',
+                discription: '변경할 닉네임 데이터 전송',
+                required : true,
+                schema:{
+                    newNickname : '철수'
+                }
+            }
+            
+            #swagger.responses[200] = {
+                description : '정상적으로 닉네임 변경 완료',
+                schema:{
+                    error : false,
+                    message : 'SUCCESS'
+                }
+            } 
+    
+            #swagger.responses[400] = {
+                description : '잘못된 데이터 전송 또는 접근으로 인한 오류 반환',
+                schema:{
+                    error: true,
+                    message : "잘못된 접근입니다."
+                }
+            }
+            #swagger.responses[500] = {
+                description : '서버 내부의 문제 발생',
+                schema:{
+                    error : true,
+                    message : "알수없은 문제 발생. 잠시후 다시 실행해주세요."
+                }
+            }
+      
+      */
+);
+
+export default router;
